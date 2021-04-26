415d9badddb75b59fa704474ddbbf6e61b76259b
halasz13@sulid.hu <halasz13@sulid.hu>
2021-04-26 17:58:58 +0200

    dependency resolved.

--- EvoNaploTFS/Migrations/20210426154831_aneve.Designer.cs
+++ EvoNaploTFS/Migrations/20210426154831_aneve.Designer.cs
@@ -4,14 +4,16 @@ using EvoNaplo.DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EvoNaplo.Migrations
 {
     [DbContext(typeof(EvoNaploContext))]
-    partial class EvoNaploContextModelSnapshot : ModelSnapshot
+    [Migration("20210426154831_aneve")]
+    partial class aneve
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
